19d7ba17435281877a45013720c52a1987b95292
Jason Anderson <jasonanderson@uchicago.edu>
2020-04-30 18:48:49 -0500

    Fixup OpenID patch and restructure code
    Change-Id: I45850398595e84d85593179159c8e52947d7b361

--- ansible/roles/keystone/files/openid_gen_metadata_and_certs.py
+++ ansible/roles/keystone/files/openid_gen_metadata_and_certs.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -13,7 +14,9 @@
 # This module creates a list of cron intervals for a node in a group of nodes
 # to ensure each node runs a cron in round robbin style.
 
+import argparse
 import json
+import os
 import re
 import requests
 import sys
@@ -25,60 +28,92 @@ else:
 
 
 def main(argv):
-    posargs = iter(argv)
+    parser = argparse.ArgumentParser(description=(
+        'Generate mod_oidc metadata files for a given identity provider. See '
+        'https://github.com/zmartzone/mod_auth_openidc/wiki/Multiple-Providers '
+        'for examples of the files this generates.'))
 
-    idp_url = next(posargs)
-    client_id = next(posargs)
-    client_secret = next(posargs)
-    certificate = next(posargs, None)
-    key_id = next(posargs, None)
-    certificate_transformer = next(posargs, None)
-    key_transformer = next(posargs, None)
+    parser.add_argument('--output-dir', required=True,
+                        help=('The directory to output the metadata/cert files '
+                              'into.'))
+    parser.add_argument('--identity-provider-url', required=True,
+                        help=('The fully qualified hostname of the remote '
+                              'identity provider.'))
+    parser.add_argument('--client-id', required=True,
+                        help='the OpenID client public ID')
+    parser.add_argument('--client-secret', required=True,
+                        help='the OpenID client secret')
+    parser.add_argument('--jwt-certificate-path',
+                        help=('The fully qualified names of the files that '
+                              'contain the X.509 certificates with the RSA '
+                              'public keys that can be used for local JWT '
+                              'access token verification.'))
+    parser.add_argument('--jwt-certificate-transformer', help='')
+    parser.add_argument('--jwt-key-path', help='')
+    parser.add_argument('--jwt-key-transformer', help='')
 
+    args = parser.parse_args(argv)
+
+    output_dir = args.output_dir
+    idp_url = args.identity_provider_url
+    client_id = args.client_id
+    client_secret = args.client_secret
+    jwt_certificate_path = args.jwt_certificate_path
+    jwt_certificate_transformer = args.jwt_certificate_transformer
+    jwt_key_path = args.jwt_key_path
+    jwt_key_transformer = args.jwt_key_transformer
+
+    metadata_dir = os.path.join(output_dir, "metadata")
+    certificate_dir = os.path.join(output_dir, "cert")
     file_name = quote_plus(re.sub("https?://", "", idp_url))
     json_provider_url = idp_url + '/.well-known/openid-configuration'
 
-    json_provider = json.dumps(requests.get(json_provider_url).json())
+    json_provider = requests.get(json_provider_url).json()
+    jwks_uri = json_provider.get("jwks_uri")
+
     # This variable is an empty json because we are not overriding any configuration
     # and the apache2 OIDC plugin needs an existing config file with a valid json,
     # even if this JSON is an empty one.
     json_conf = {}
-    json_client = json.dumps({'client_id': client_id, 'client_secret': client_secret})
+    json_client = {'client_id': client_id, 'client_secret': client_secret}
 
-    if key_id and certificate:
-        key_id = get_value_by_url(key_id)
-        if key_transformer:
-            key_id = eval(key_transformer)
-        certificate = get_value_by_url(certificate)
-        if certificate_transformer:
-            certificate = eval(certificate_transformer)
-    elif not (key_id or certificate):
-        jwks_uri = json_provider.get("jwks_uri")
-        if jwks_uri:
-            key_id, certificate = fetch_jkws_key(jwks_uri)
-    else
-        raise ValueError(
-            "Either both a key_id and a certificate must be specified, or else "
-            "neither, in which case the values are automatically inferred from "
-            "the IdP well-known metadata URL.")
+    if not jwt_key_path:
+        jwt_key_path = jwks_uri
+    if not jwt_certificate_path:
+        jwt_certificate_path = jwks_uri
 
-    create_file("metadata", file_name, "provider", json_provider)
-    create_file("metadata", file_name, "client", json_client)
-    create_file("metadata", file_name, "conf", json_conf)
+    jwt_key = get_value_by_url(jwt_key_path)
+    jwt_certificate = get_value_by_url(jwt_certificate_path)
 
-    if key_id and certificate:
-        create_file("cert", key_id, "pem", certificate)
-        print(key_id)
+    if jwt_certificate_transformer:
+        jwt_certificate = eval(jwt_certificate_transformer)
+    else:
+        jwt_certificate = default_jwt_certificate_transformer(jwt_certificate)
+
+    if jwt_key_transformer:
+        jwt_key = eval(jwt_key_transformer)
+    else:
+        jwt_key = default_jwt_key_transformer(jwt_key)
+
+    create_file(metadata_dir, file_name, "provider", json.dumps(json_provider))
+    create_file(metadata_dir, file_name, "client", json.dumps(json_client))
+    create_file(metadata_dir, file_name, "conf", json.dumps(json_conf))
+
+    if jwt_key and jwt_certificate:
+        create_file(certificate_dir, jwt_key, "pem", jwt_certificate)
+        print(jwt_key)
 
 
-def fetch_jkws_key(jwks_uri):
-    certs = json.dumps(requests.get(jwks_uri))
-    keys = certs.get("keys")
+def default_jwt_key_transformer(jwt_key):
+    keys = json.loads(jwt_key).get("keys")
 
-    if not keys:
-        return None, None
+    return keys[0]["kid"] if keys else None
 
-    return keys[0]["kid"], keys[0]["x5c"][0]
+
+def default_jwt_certificate_transformer(jwt_certificate):
+    keys = json.loads(jwt_certificate).get("keys")
+
+    return keys[0]["x5c"][0] if keys else None
 
 
 def create_file(file_path, file_name, extension, content):
